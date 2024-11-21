dfb5e8a79ed929ae8558e0b50e4e6386c70027b2
Victor Freitag <132359912+victorfreitag@users.noreply.github.com>
2024-11-21 16:19:45 -0300

    blog

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CardsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Blog';
+}
